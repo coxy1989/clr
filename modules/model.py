340a883b5d7d449af94cc3a2ed6f311683b6609c
Jamie Cox <jc8478@me.com>
2019-01-07 15:21:58 +0000

    update models

--- modules/model.py
+++ modules/model.py
@@ -5,8 +5,38 @@ def init_weights(module:nn.Module):
         nn.init.normal_(module.weight.data, std=0.01, mean=0)
         nn.init.constant_(module.bias.data, 0)
 
-class Cifar10Net(nn.Module):
-    'TODO: docstring'
+class LRN(nn.Module):
+    'Implementation of local response normalisation. The pytorch implementation does not afford
+    within channel normalisation - https://github.com/pytorch/pytorch/issues/653'
+
+    def __init__(self, local_size=1, alpha=1.0, beta=0.75, ACROSS_CHANNELS=False):
+        super(LRN, self).__init__()
+        self.ACROSS_CHANNELS = ACROSS_CHANNELS
+        if self.ACROSS_CHANNELS:
+            self.average=nn.AvgPool3d(kernel_size=(local_size, 1, 1), 
+                    stride=1,
+                    padding=(int((local_size-1.0)/2), 0, 0)) 
+        else:
+            self.average=nn.AvgPool2d(kernel_size=local_size,
+                    stride=1,
+                    padding=int((local_size-1.0)/2))
+        self.alpha = alpha
+        self.beta = beta
+
+    def forward(self, x):
+        if self.ACROSS_CHANNELS:
+            div = x.pow(2).unsqueeze(1)
+            div = self.average(div).squeeze(1)
+            div = div.mul(self.alpha).add(1.0).pow(self.beta)
+        else:
+            div = x.pow(2)
+            div = self.average(div)
+            div = div.mul(self.alpha).add(1.0).pow(self.beta)
+        x = x.div(div)
+        return x
+
+class Cifar10Net_quick(nn.Module):
+    'Port of caffe architecture: https://github.com/BVLC/caffe/blob/master/examples/cifar10/cifar10_quick_train_test.prototxt'
 
     def __init__(self, num_classes=10):
         'TODO: docstring'
@@ -18,6 +48,8 @@ class Cifar10Net(nn.Module):
         self.relu2 = nn.ReLU()
         self.pool2 = nn.AvgPool2d(kernel_size=3, stride=2)
         self.conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=5, padding=2)
+        self.relu3 = nn.ReLU()
+        self.pool3 = nn.AvgPool2d(kernel_size=3, stride=2)
         self.ip1 = nn.Linear(64 * 7 * 7, 64)
         self.ip2 = nn.Linear(64, 10)
 
@@ -30,6 +62,45 @@ class Cifar10Net(nn.Module):
         out = self.relu2(out)
         out = self.pool2(out)
         out = self.conv3(out)
+        out = self.relu3(out)
+        out = self.pool3(out)
+        out = self.ip1(out.view(out.shape[0], out.shape[1] * out.shape[2] * out.shape[3]))
+        out = self.ip2(out)
+        return out
+
+class Cifar10Net_full(nn.Module):
+    'Port of caffe architecture: https://github.com/BVLC/caffe/blob/master/examples/cifar10/cifar10_quick_train_test.prototxt'
+
+    def __init__(self, num_classes=10):
+        'TODO: docstring'
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=5, padding=2)
+        self.pool1 = nn.MaxPool2d(kernel_size=3, stride=2)
+        self.relu1 = nn.ReLU()
+        self.norm1 = nn.LRN(local_size=3, alpha=5e-5, beta=0.75)
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=5, padding=2)
+        self.relu2 = nn.ReLU()
+        self.pool2 = nn.AvgPool2d(kernel_size=3, stride=2)
+        self.norm2 = nn.LRN(local_size=3, alpha=5e-5, beta=0.75)
+        self.conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=5, padding=2)
+        self.relu3 = nn.ReLU()
+        self.pool3 = nn.AvgPool2d(kernel_size=3, stride=2)
+        self.ip1 = nn.Linear(64 * 7 * 7, 64)
+        self.ip2 = nn.Linear(64, 10)
+
+    def forward(self, x):
+        'TODO: docstring'
+        out = self.conv1(x)
+        out = self.pool1(out)
+        out = self.relu1(out)
+        out = self.norm1(out)
+        out = self.conv2(out)
+        out = self.relu2(out)
+        out = self.pool2(out)
+        out = self.norm2(out)
+        out = self.conv3(out)
+        out = self.relu3(out)
+        out = self.pool3(out)
         out = self.ip1(out.view(out.shape[0], out.shape[1] * out.shape[2] * out.shape[3]))
         out = self.ip2(out)
         return out
